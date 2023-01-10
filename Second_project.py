cfbe44425956fdd1be9a4f4618bec0993252b02f
P00jan <poojan2132000p@gmail.com>
2023-01-10 16:58:10 -0500

    SP

---
+++ Second_project.py
@@ -0,0 +1,8 @@
+"""
+Commission Calculator using Logical operators
+"""
+
+
+Total_amt = int(input("Enter the total amount:"))
+commission = (Total_amt * 13) / 100
+print("The total commission is" , commission)
